186f3b457aefbde8c801b06a27f1c931bf82daf3
jinglebot <jingle1216@yahoo.com>
2016-08-18 08:52:16 -0700

    Cat Clicker Premium with 10 cats

--- js/script.js
+++ js/script.js
@@ -1,63 +1,104 @@
-    // For clicking the cat
-    // jQuery version
-    // $('#xaxa').click(function(e) {
-    //     // add 1 to counter everytime cat picture is clicked
-    //     ctr++;
-    //     $('#counter').text('Clicks : ' + ctr);
-    // });
-
-
 var cats =
     [{
-        "name": "Xaxa",
+        "name": "Alpha",
+        "img": ["images/cat_0.jpg"],
+        "ctr": 0
+    },{
+        "name": "Buffy",
         "img": ["images/cat_1.jpg"],
         "ctr": 0
     },{
-        "name": "Chewie",
+        "name": "Copper",
         "img": ["images/cat_2.jpg"],
+        "ctr": 0
+    },{
+        "name": "Darling",
+        "img": ["images/cat_3.jpg"],
+        "ctr": 0
+    },{
+        "name": "Earl",
+        "img": ["images/cat_4.jpg"],
+        "ctr": 0
+    },{
+        "name": "Fluffy",
+        "img": ["images/cat_5.jpg"],
+        "ctr": 0
+    },{
+        "name": "Gorgeous",
+        "img": ["images/cat_6.jpg"],
+        "ctr": 0
+    },{
+        "name": "Happy",
+        "img": ["images/cat_7.jpg"],
+        "ctr": 0
+    },{
+        "name": "Iffy",
+        "img": ["images/cat_8.jpg"],
+        "ctr": 0
+    },{
+        "name": "Jollibee",
+        "img": ["images/cat_9.jpg"],
         "ctr": 0
     }]
 ;
 
 
-var formattedCatname = '<div><h1 class="catName">%data%</h1></div>';
-var formattedCatctr = '<div><h2 class="catCtr">%data%</h2></div>';
-var formattedCatimg = '<div><img class="catImg" class="img-responsive" style="max-width: 100%" src="%data%" sizes="100w" alt="%alt%"></div>';
+var formattedUlHtmlStart = '<div class="col-sm-3"><ul id="catList"></ul></div>';
+var formattedLiHtmlStart = '<li id="catItem">%data%</li>';
 
+var formattedDisplayHtmlStart = '<div class="col-sm-9" id="catDisplay"></div>';
+var formattedDataHtmlStart = '<div id="catData">%data%</div>';
+
+var formattedCatname = '<div class="col-sm-6" id="catName">%data%</div>';
+var formattedCatctr = '<div class="col-sm-6" id="catCtr">%data%</div>';
+var formattedCatimg = '<img id="catImg" class="img-responsive" style="max-width: 100%" src="%data%" sizes="100w" alt="%alt%">';
 
 function display() {
-    // var profile = [];
+
+    // initial catList
+    $('#cat').append(formattedUlHtmlStart);
     for (var i = 0; i < cats.length; i++) {
         var catcopy = cats[i];
         (function (catcopy) {
             var htmlCatId = catcopy.name;
-            var profile = '<div class="htmlCat" id=' + htmlCatId + ' style="text-align:center"></div>';
-            var htmlCatname = formattedCatname.replace("%data%", catcopy.name);
-            var htmlCatctr = formattedCatctr.replace("%data%", catcopy.ctr);
-            var htmlCatimg = formattedCatimg.replace("%data%", catcopy.img[0]);
-                // htmlCatimg = formattedCatimg.replace("%alt%", cats[icopy].name);
-
-            $('#cat').append(profile);
-            $('.htmlCat:last').append(htmlCatname);
-            $('.htmlCat:last').append(htmlCatctr);
-            $('.htmlCat:last').append(htmlCatimg);
+            var htmlCatname = formattedLiHtmlStart.replace("%data%", catcopy.name);
+            htmlCatname = htmlCatname.replace("catItem", catcopy.name);
+            $('#catList:last').append(htmlCatname);
         })(catcopy)
     }
 
-    // To get counter to count clicks
-    var catImage = document.getElementsByClassName("catImg");
+    // initial catDisplay
+    $('#cat').append(formattedDisplayHtmlStart);
+    var htmlCatname = formattedCatname.replace("%data%", cats[0].name);
+    var htmlCatctr = formattedCatctr.replace("%data%", "Click count : " + cats[0].ctr);
+    var htmlCatData = formattedDataHtmlStart.replace("%data%", htmlCatname + htmlCatctr);
+    var htmlCatimg = formattedCatimg.replace("%data%", cats[0].img[0]);
+    $('#catDisplay').append(htmlCatData);
+    $('#catDisplay').append(htmlCatimg);
 
-    var modifyCtr = function(numcopy) {
-        // var attribute = this.siblings("catCtr").text("add one");
-        // console.log(numcopy);
-        // console.log(this.parentNode.className);
+    // modify catDisplay function
+    var modifyCtr = function() {
+        // console.log(this.id);
+        // console.log(this);
+        var catPic = document.getElementById("catImg");
+        $('#catName').text(this.id);
+        for (var i = 0; i < cats.length; i++) {
+            var catcopy = cats[i];
+            if (catcopy.name == this.id) {
+                catcopy.ctr = catcopy.ctr + 1;
+                // console.log (catcopy.name + catcopy.ctr);
+                $('#catCtr').text("Click count : " + catcopy.ctr);
+                catPic.src = catcopy.img;
+            }
+        }
     };
 
-    for (var i = 0; i < catImage.length; i++) {
-        var numcopy = catImage[i];
-        var htmlCatmom = $(numcopy).parentNode;
-        console.log(htmlCatmom);
-        catImage[i].onclick = modifyCtr(numcopy);
+    // Event Listener for catImgs
+    for (var i = 0; i < cats.length; i++) {
+        var catcopy = cats[i];
+        var currentCat = document.getElementById(catcopy.name);
+        currentCat.addEventListener("click", modifyCtr, false);
+        // console.log(currentCat.id);
     }
 }
 
@@ -65,57 +106,3 @@ if (cats.length > 0) {
         display();
 }
 
-
-// 1. counter adds 1
-// 2. counter on screen changes value
-
-// function modifyCtr() {
-//     console.log("clicked");
-// }
-// var catClick = document.getElementById("catImg");
-// catClick.addEventListener("click", modifyCtr, false);
-
-// var catImage = document.getElementsByClassName("catImg");
-
-// var modifyCtr = function() {
-//     // var attribute = this.siblings("catCtr").text("add one");
-//     console.log(this.parentNode.className);
-// };
-
-// for (var i = 0; i < catImage.length; i++) {
-//     catImage[i].addEventListener('click', modifyCtr, false);
-// }
-
-//     for (var i = 0; i < cats.length; i++) {
-//         var numcopy = i;
-//         (function(numcopy){
-//             var mommy = document.('#catImg').parent();
-//     console.log(mommy);
-
-//         })(i)
-//     //     if (mommy == cats[i].name) {
-//     //         cats[i].ctr++;
-//     //         mommy.getElementById("catCtr").text('Clicks : ' + cats[i].ctr);
-//         e.target.siblings("#catCtr").text('Clicks : ' + cats[i].ctr);
-//     }
-// })
-
-
-// $('#catImg').click(function(e) {
-    // add 1 to counter everytime cat picture is clicked
-    // var mommy = document.('#catImg').parent();
-    // console.log(mommy);
-    // for (var i = 0; i < cats.length; i++) {
-    //     if (mommy == cats[i].name) {
-    //         cats[i].ctr++;
-    //         mommy.getElementById("catCtr").text('Clicks : ' + cats[i].ctr);
-    //     }
-    // }
-// })
-
-
-    // Vanilla JS version
-    // elem.addEventListener('click', function(){
-    // ctr++;
-    // $('#counter').text('Clicks : ' + ctr);
-    // }, false);
