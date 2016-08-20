567bcec7f7084e15a435cb4b1a742f6d32b16ccb
jinglebot <jingle1216@yahoo.com>
2016-08-20 14:58:04 -0700

    Cat Clicker Premium MVO

--- js/script.js
+++ js/script.js
@@ -1,128 +1,135 @@
-// MODEL
-var cats =
-    [{
-        "name": "Alpha",
-        "img": ["images/cat_0.jpg"],
-        "ctr": 0
-    },{
-        "name": "Buffy",
-        "img": ["images/cat_1.jpg"],
-        "ctr": 0
-    },{
-        "name": "Copper",
-        "img": ["images/cat_2.jpg"],
-        "ctr": 0
-    },{
-        "name": "Darling",
-        "img": ["images/cat_3.jpg"],
-        "ctr": 0
-    },{
-        "name": "Earl",
-        "img": ["images/cat_4.jpg"],
-        "ctr": 0
-    },{
-        "name": "Fluffy",
-        "img": ["images/cat_5.jpg"],
-        "ctr": 0
-    },{
-        "name": "Gorgeous",
-        "img": ["images/cat_6.jpg"],
-        "ctr": 0
-    },{
-        "name": "Happy",
-        "img": ["images/cat_7.jpg"],
-        "ctr": 0
-    },{
-        "name": "Iffy",
-        "img": ["images/cat_8.jpg"],
-        "ctr": 0
-    },{
-        "name": "Jollibee",
-        "img": ["images/cat_9.jpg"],
-        "ctr": 0
-    }]
-;
+$(function() {
 
-// VIEW
-var formattedUlHtmlStart = '<div class="col-sm-3"><ul id="catList"></ul></div>';
-var formattedLiHtmlStart = '<li id="catItem">%data%</li>';
+    // MODEL
+    var cats =
+        [{
+            "name": "Alpha",
+            "img": ["images/cat_0.jpg"],
+            "ctr": 0
+        },{
+            "name": "Buffy",
+            "img": ["images/cat_1.jpg"],
+            "ctr": 0
+        },{
+            "name": "Copper",
+            "img": ["images/cat_2.jpg"],
+            "ctr": 0
+        },{
+            "name": "Darling",
+            "img": ["images/cat_3.jpg"],
+            "ctr": 0
+        },{
+            "name": "Earl",
+            "img": ["images/cat_4.jpg"],
+            "ctr": 0
+        },{
+            "name": "Fluffy",
+            "img": ["images/cat_5.jpg"],
+            "ctr": 0
+        },{
+            "name": "Gorgeous",
+            "img": ["images/cat_6.jpg"],
+            "ctr": 0
+        },{
+            "name": "Happy",
+            "img": ["images/cat_7.jpg"],
+            "ctr": 0
+        },{
+            "name": "Iffy",
+            "img": ["images/cat_8.jpg"],
+            "ctr": 0
+        },{
+            "name": "Jollibee",
+            "img": ["images/cat_9.jpg"],
+            "ctr": 0
+        }]
+    ;
 
-var formattedDisplayHtmlStart = '<div class="col-sm-9" id="catDisplay"></div>';
-var formattedDataHtmlStart = '<div id="catData">%data%</div>';
+    // VIEW
+    var view = {
+        init: function() {
 
-var formattedCatname = '<div class="col-sm-6" id="catName">%data%</div>';
-var formattedCatctr = '<div class="col-sm-6" id="catCtr">%data%</div>';
-var formattedCatimg = '<img id="catImg" class="img-responsive" style="max-width: 100%" src="%data%" sizes="100w" alt="%alt%">';
+            var formattedUlHtmlStart = '<div class="col-sm-3"><ul id="catList"></ul></div>';
+            var formattedLiHtmlStart = '<li id="catItem">%data%</li>';
 
-// VIEW
-function display() {
+            var formattedDisplayHtmlStart = '<div class="col-sm-9" id="catDisplay"></div>';
+            var formattedDataHtmlStart = '<div id="catData">%data%</div>';
 
-    // initial catList
-    $('#cat').append(formattedUlHtmlStart);
-    for (var i = 0; i < cats.length; i++) {
-        var catcopy = cats[i];
-        (function (catcopy) {
-            var htmlCatId = catcopy.name;
-            var htmlCatname = formattedLiHtmlStart.replace("%data%", catcopy.name);
-            htmlCatname = htmlCatname.replace("catItem", catcopy.name);
-            $('#catList:last').append(htmlCatname);
-        })(catcopy)
-    }
+            var formattedCatname = '<div class="col-sm-6" id="catName">%data%</div>';
+            var formattedCatctr = '<div class="col-sm-6" id="catCtr">%data%</div>';
+            var formattedCatimg = '<img id="catImg" class="img-responsive" style="max-width: 100%" src="%data%" sizes="100w" alt="%alt%">';
 
-    // initial catDisplay
-    $('#cat').append(formattedDisplayHtmlStart);
-    var htmlCatname = formattedCatname.replace("%data%", cats[0].name);
-    var htmlCatctr = formattedCatctr.replace("%data%", "Click count : " + cats[0].ctr);
-    var htmlCatData = formattedDataHtmlStart.replace("%data%", htmlCatname + htmlCatctr);
-    var htmlCatimg = formattedCatimg.replace("%data%", cats[0].img[0]);
-    htmlCatimg = htmlCatimg.replace("%alt%", cats[0].name);
-    htmlCatimg = htmlCatimg.replace("catImg", "catImg_" + cats[0].name);
-    $('#catDisplay').append(htmlCatData);
-    $('#catDisplay').append(htmlCatimg);
-
-    // OCTOPUS
-    // modify catDisplay function
-    var modifyImg = function() {
-        // console.log(this);
-        var catPic = document.getElementById("catDisplay").lastChild;
-        // console.log(catPic);
-        $('#catName').text(this.id);
-        for (var i = 0; i < cats.length; i++) {
-            var catcopy = cats[i];
-            if (catcopy.name == this.id) {
-                $('#catCtr').text("Click count : " + catcopy.ctr);
-                catPic.id = "catImg_" + catcopy.name;
-                catPic.src = catcopy.img;
-                catPic.alt = catcopy.name;
+            // initial catList
+            $('#cat').append(formattedUlHtmlStart);
+            for (var i = 0; i < cats.length; i++) {
+                var catcopy = cats[i];
+                (function (catcopy) {
+                    var htmlCatId = catcopy.name;
+                    var htmlCatname = formattedLiHtmlStart.replace("%data%", catcopy.name);
+                    htmlCatname = htmlCatname.replace("catItem", catcopy.name);
+                    $('#catList:last').append(htmlCatname);
+                })(catcopy)
             }
-        }
+
+            // initial catDisplay
+            $('#cat').append(formattedDisplayHtmlStart);
+            var htmlCatname = formattedCatname.replace("%data%", cats[0].name);
+            var htmlCatctr = formattedCatctr.replace("%data%", "Click count : " + cats[0].ctr);
+            var htmlCatData = formattedDataHtmlStart.replace("%data%", htmlCatname + htmlCatctr);
+            var htmlCatimg = formattedCatimg.replace("%data%", cats[0].img[0]);
+            htmlCatimg = htmlCatimg.replace("%alt%", cats[0].name);
+            htmlCatimg = htmlCatimg.replace("catImg", "catImg_" + cats[0].name);
+
+            $('#catDisplay').append(htmlCatData);
+            $('#catDisplay').append(htmlCatimg);
+        },
     };
 
-    // modify number of clicks on image function
-    var modifyCtr = function() {
-        console.log(this.id);
-        for (var i = 0; i < cats.length; i++) {
-            var catcopy = cats[i];
-            if (("catImg_" + catcopy.name) == this.id) {
-                catcopy.ctr = catcopy.ctr + 1;
-                $('#catCtr').text("Click count : " + catcopy.ctr);
+    var octopus = {
+
+        modifyImg: function() {
+            // console.log(this.id);
+            var catPic = document.getElementById("catDisplay").lastChild;
+            $('#catName').text(this.id);
+            for (var i = 0; i < cats.length; i++) {
+                var catcopy = cats[i];
+                if (catcopy.name == this.id) {
+                    $('#catCtr').text("Click count : " + catcopy.ctr);
+                    catPic.id = "catImg_" + catcopy.name;
+                    catPic.src = catcopy.img;
+                    catPic.alt = catcopy.name;
+                }
+            }
+        },
+
+        modifyCtr: function() {
+            // console.log("modifyCtr: " + this.id);
+            for (var i = 0; i < cats.length; i++) {
+                var catcopy = cats[i];
+                if (("catImg_" + catcopy.name) == this.id) {
+                    catcopy.ctr = catcopy.ctr + 1;
+                    $('#catCtr').text("Click count : " + catcopy.ctr);
+                }
+            }
+        },
+
+        init: function() {
+            view.init();
+
+            var currentPic = document.getElementById("catImg_" + cats[0].name);
+            currentPic.addEventListener("click", octopus.modifyCtr, false);
+            // console.log(currentPic);
+
+            for (var i = 0; i < cats.length; i++) {
+                var catcopy = cats[i];
+                var currentCat = document.getElementById(catcopy.name);
+                currentCat.addEventListener("click", octopus.modifyImg, false);
+                // console.log(currentCat);
             }
         }
-    }
 
-    // Event Listener for catImgs
-    var currentPic = document.getElementById("catImg_" + cats[0].name);
-    currentPic.addEventListener("click", modifyCtr, false);
-    // console.log(currentPic);
+    };
 
-    for (var i = 0; i < cats.length; i++) {
-        var catcopy = cats[i];
-        var currentCat = document.getElementById(catcopy.name);
-        currentCat.addEventListener("click", modifyImg, false);
-    }
-}
+    octopus.init();
 
-if (cats.length > 0) {
-        display();
-}
-
+}());
